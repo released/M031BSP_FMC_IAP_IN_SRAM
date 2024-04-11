222090baaf8116ab79df0f45a4eea8fe5e973b86
Released <released.chen@gmail.com>
2024-04-11 13:59:18 +0800

    Update update_flash.h

--- SampleCode/Template/update_flash/update_flash.h
+++ SampleCode/Template/update_flash/update_flash.h
@@ -3,20 +3,10 @@
 #include "NuMicro.h"
 
 /*_____ D E C L A R A T I O N S ____________________________________________*/
-#define UPDATE_AP_PACKET_INFO_SIZE                 (6)
-#define UPDATE_AP_PACKET_DATA_SIZE                 (32)
-#define UPDATE_AP_PACKET_TOTAL_SIZE                (UPDATE_AP_PACKET_INFO_SIZE+UPDATE_AP_PACKET_DATA_SIZE)
-
-
 #define APP1_ADDR                                  (FMC_APROM_BASE)
 #define APP1_SIZE                                  (60*1024)   
 #define APP2_ADDR                                  (60*1024)   // 0xF000
 #define APP2_SIZE                                  (60*1024)   
-
-// #define APP1_ADDR                                  (30*1024)   // 0x7800
-// #define APP1_SIZE                                  (30*1024)   
-// #define APP2_ADDR                                  (60*1024)   // 0xF000
-// #define APP2_SIZE                                  (30*1024)   
 
 /*_____ D E F I N I T I O N S ______________________________________________*/
 
